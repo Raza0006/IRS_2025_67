061cb325b0aca7920d523537fad7db1929a04f93
Raza0006 <r.k.pasha.aman@gmail.com>
2025-09-17 10:36:37 +0000

    Fixed hmi listener

--- pa_warehouse_status/pa_warehouse_status/plc_hmi_listener.py
+++ pa_warehouse_status/pa_warehouse_status/plc_hmi_listener.py
@@ -1,4 +1,5 @@
 import rclpy
+import json
 from rclpy.node import Node
 from std_msgs.msg import String
 
@@ -7,7 +8,7 @@ class   StatusUpdateSubscriber(Node):
         super().__init__('StatusUpdateSubscriber')  # initialise node with name of the node
         self.subscription = self.create_subscription(
             String,
-            '/status_updates',
+            '/hmi/unified_status',
             self.subscriber_callback,
             10
         )
@@ -15,8 +16,20 @@ class   StatusUpdateSubscriber(Node):
         self.i = 0 #callback counter
 
     def subscriber_callback(self, msg):
-        self.get_logger().info(f'Recieved "{msg.data}" {self.i} times')
-        self.i += 1
+        try:
+            data = json.loads(msg.data) # parse JSON string into Python dict
+            stamp = data["stamp"]
+            box = data["box"]
+            counts = data["counts"]
+            print("Received PLC status:")
+            print(f"Time: {stamp['sec']}.{stamp['nanosec']}")
+            print(f"Box weight raw={box['weight_raw']}")
+            print(f" Location: {box['location']}")
+            print(f" Counts: big={counts['big']}, medium={counts['medium']}, "
+            f"small={counts['small']}, total={counts['total']}")
+            print() # empty line at the end
+        except Exception as e:
+            self.get_logger().error(f"Failed to parse JSON: {e}\nRaw msg={msg.data}")
 
 def main(args=None):
     rclpy.init(args=args)
